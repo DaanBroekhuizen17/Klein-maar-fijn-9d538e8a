3bdb02f4923b19cbc618e2ece6090b93f03b2855
daan <2087884@talnet.nl>
2020-02-13 15:24:05 +0100

    klaar ermee

---
+++ wisselgeld.php
@@ -0,0 +1,26 @@
+<?php
+
+$input = $argv[1];
+$bedrag = intval($input);
+
+define("geldeenheden", [
+    50,
+    20,
+    10,
+    5,
+    2,
+    1
+    ]);
+    
+
+$restbedrag = $bedrag;
+
+foreach(geldeenheden as $geldeenheid){
+    if($restbedrag >= $geldeenheid){
+        $aantalKeerGeldEenheidInRestBedrag = round($restbedrag / $geldeenheid);
+        $restbedrag = $restbedrag % $geldeenheid;
+        echo($aantalKeerGeldEenheidInRestBedrag. " keer " .$geldeenheid. " euro".PHP_EOL);
+    }
+}
+
+?>
